3cb24bcf4a9263139aceb87ca2efca91d01d1bb1
Dell-User <Dell-User@DESKTOP-221INCO>
2024-01-24 11:05:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Class_Methods
+{
+    internal class Program
+    {
+        internal class Student 
+        {
+            public double weight;
+            public int age;
+            public string name;
+
+            public Student(double aWeight, string aName, int aAge) 
+            {
+              this.weight = aWeight;
+                this.age = aAge;
+                this.name = aName;
+            }
+            //Now we can test obesity for the students
+            public bool IsObese()
+            { 
+            if (weight > 60)
+                {
+                    return true;
+                }
+                 else return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(70, "John", 15);
+            Student student2 = new Student(40, "Tim", 10);
+            Console.WriteLine(student1.IsObese());
+            Console.WriteLine(student2.IsObese());
+
+            Console.ReadLine();
+
+        }
+    }
+}
